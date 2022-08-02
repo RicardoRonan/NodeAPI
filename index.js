ad02a5306c43bc4a0614257f45ee085a7f93dd74
RicardoRonan <ricardoronanm@gmail.com>
2022-08-02 16:34:17 +0200

    added frontend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import needed libraries
 const express = require("express"); // Used to set up a server
 const cors = require("cors"); // Used to prevent errors when working locally
+const bodyParser = require("body-parser");
 
 // Import routes
 const userRoute = require("./routes/userRoute");
@@ -13,11 +14,14 @@ const app = express(); // Initialize express as an app variable
 app.set("port", process.env.PORT || 6969); // Set the port
 app.use(express.json()); // Enable the server to handle JSON requests
 app.use(cors()); // Dont let local development give errors
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // This is where we check URLs and Request methods to create functionality
 // GET '/' is always what will be displayed on the home page of your application
 app.get("/", (req, res) => {
-  res.json({ msg: "Welcome" });
+  res.sendFile(__dirname + "/" + "index.html");
 });
 // Use individual routes when visiting these URLS
 app.use("/users", userRoute);
@@ -30,3 +34,6 @@ app.listen(app.get("port"), () => {
   console.log(`Listening for calls on port ${app.get("port")}`);
   console.log("Press Ctrl+C to exit server");
 });
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap";
+app.use(express.static("public"));
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,11 +5,13 @@ const middleware = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-router.get("/", middleware, (req, res) => {
+const authController = require("../controller/Auth/index");
+// get all
+router.get("/", (req, res) => {
   try {
     con.query("SELECT * FROM users", (err, result) => {
       if (err) throw err;
-      res.send(result);
+      res.json(result);
     });
   } catch (error) {
     console.log(error);
@@ -17,7 +19,7 @@ router.get("/", middleware, (req, res) => {
 });
 
 // Gets one users
-router.get("/:id", (req, res) => {
+router.get("/:id", middleware, (req, res) => {
   try {
     con.query(
       `SELECT * FROM users where user_id =${req.params.id} `,
@@ -107,89 +109,88 @@ router.delete("/:id", (req, res) => {
 // Register Route
 // The Route where Encryption starts
 router.post("/register", (req, res) => {
-  try {
-    let sql = "INSERT INTO users SET ?";
-    const {
-      full_name,
-      email,
-      password,
-      user_type,
-      phone,
-      country,
-      billing_address,
-      default_shipping_address,
-    } = req.body;
-
-    // The start of hashing / encryption
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-
-    let user = {
-      full_name,
-      email,
-      // We sending the hash value to be stored witin the table
-      password: hash,
-      user_type,
-      phone,
-      country,
-      billing_address,
-      default_shipping_address,
-    };
-    con.query(sql, user, (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      res.send(`User ${(user.full_name, user.email)} created successfully`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  return authController.Register(req, res);
 });
 
 // Login
 router.post("/login", (req, res) => {
+  return authController.Login(req, res);
+});
+
+// Verify
+router.get("/users/verify", (req, res) => {
+  const token = req.header("x-auth-token");
+  return authController.Verify(req, res);
+});
+// Importing the dependencies
+const nodemailer = require("nodemailer");
+
+router.post("/forgot-psw", (req, res) => {
   try {
     let sql = "SELECT * FROM users WHERE ?";
     let user = {
       email: req.body.email,
     };
-    con.query(sql, user, async (err, result) => {
+    con.query(sql, user, (err, result) => {
       if (err) throw err;
-      if (result.length === 0) {
-        res.send("Email not found please register");
+      if (result === 0) {
+        res.status(400), res.send("Email not found");
       } else {
-        const isMatch = await bcrypt.compare(
-          req.body.password,
-          result[0].password
-        );
-        if (!isMatch) {
-          res.send("Password incorrect");
-        } else {
-          // The information the should be stored inside token
-          const payload = {
-            user: {
-              user_id: result[0].user_id,
-              full_name: result[0].full_name,
-              email: result[0].email,
-              user_type: result[0].user_type,
-              phone: result[0].phone,
-              country: result[0].country,
-              billing_address: result[0].billing_address,
-              default_shipping_address: result[0].default_shipping_address,
-            },
-          };
-          // Creating a token and setting expiry date
-          jwt.sign(
-            payload,
-            process.env.jwtSecret,
-            {
-              expiresIn: "365d",
-            },
-            (err, token) => {
-              if (err) throw err;
-              res.json({ token });
-            }
-          );
-        }
+        // Allows me to connect to the given email account || Your Email
+
+        const transporter = nodemailer.createTransport({
+          host: "smtp.ethereal.email",
+          port: 587,
+          auth: {
+            user: "jasper.heidenreich0@ethereal.email",
+            pass: "mFWbw71Rk5MTnzT4g9",
+          },
+        });
+
+        // How the email should be sent out
+        var mailData = {
+          from: process.env.MAILERUSER,
+          // Sending to the person who requested
+          to: result[0].email,
+
+          subject: "Password Reset",
+          html: `<div>
+            <h3>Hi ${result[0].full_name},</h3>
+            <br>
+            <h4>Click link below to reset your password</h4>
+
+            <a href="http://localhost:6969/resetpsw.html">
+              Click Here to Reset Password
+              user_id = ${result[0].user_id}
+            </a>
+
+            <br>
+            <p>For any queries feel free to contact us...</p>
+            <div>
+              Email: ${process.env.MAILERUSER}
+              <br>
+              Tel: If needed you can add this
+            <div>
+          </div>`,
+        };
+
+        // Check if email can be sent
+        // Check password and email given in .env file
+        transporter.verify((error, success) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email valid! ", success);
+          }
+        });
+
+        transporter.sendMail(mailData, (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            res.send("Please Check your email", result[0].user_id);
+          }
+        });
       }
     });
   } catch (error) {
@@ -197,17 +198,41 @@ router.post("/login", (req, res) => {
   }
 });
 
-// Verify
-router.get("/users/verify", (req, res) => {
-  const token = req.header("x-auth-token");
-  jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
-    if (error) {
-      res.status(401).json({
-        msg: "Unauthorized Access!",
-      });
+// Rest Password Route
+
+router.put("reset-psw/:id", (req, res) => {
+  let sql = "SELECT * FROM users WHERE ?";
+  let user = {
+    user_id: req.params.id,
+  };
+  con.query(sql, user, (err, result) => {
+    if (err) throw err;
+    if (result === 0) {
+      res.status(400), res.send("User not found");
     } else {
-      res.status(200);
-      res.send(decodedToken);
+      let newPassword = `UPDATE users SET ? WHERE user_id = ${req.params.id}`;
+
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+
+      const updatedPassword = {
+        full_name: result[0].full_name,
+        email: result[0].email,
+        user_type: result[0].user_type,
+        phone: result[0].phone,
+        country: result[0].country,
+        billing_address: result[0].billing_address,
+        default_shipping_address: result[0].default_shipping_address,
+
+        // Only thing im changing in table
+        password: hash,
+      };
+
+      con.query(newPassword, updatedPassword, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Password Updated please login");
+      });
     }
   });
 });
